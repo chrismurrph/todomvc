9b2b4378d02856aea08eef789301176226e043f4
Chris Murphy <chris@strandz.org>
2016-07-14 10:57:17 +1000

    Some generic queries that work fine

--- dev/server/user.clj
+++ dev/server/user.clj
@@ -8,6 +8,7 @@
     [figwheel-sidecar.repl-api :as ra]
     [taoensso.timbre :refer [info set-level!]]
     [todomvc.system :as system]
+    [datomic.api :as d]
     ))
 
 ;;FIGWHEEL
@@ -57,6 +58,35 @@
   []
   (stop)
   (refresh :after 'user/go))
+
+(def system-ns #{"db" "db.type" "db.install" "db.part"
+                 "db.lang" "fressian" "db.unique" "db.excise"
+                 "db.cardinality" "db.fn" "db.sys"
+                 "entity" "confirmity" "datomic-toolbox"
+                 "constraint" "db.alter" "db.bootstrap"})
+
+;(def todo-db-uri "datomic:dev://localhost:4334/too")
+
+(defn db-uri [db-name]
+  (str "datomic:dev://localhost:4334/" db-name))
+
+;;datomic:dev://{transactor-host}:{port}/*
+(def any-db-uri "datomic:dev://localhost:4334/*")
+
+(defn get-database-names []
+  (d/get-database-names any-db-uri))
+
+(defn show-entities [db-name]
+  (let [conn (d/connect (db-uri db-name))
+        entities (d/q '[:find ?e ?ident
+                 :in $ ?system-ns
+                 :where
+                 [?e :db/ident ?ident]
+                 [(namespace ?ident) ?ns]
+                 [((comp not contains?) ?system-ns ?ns)]]
+               (d/db conn) system-ns)
+        _ (.release conn)]
+    entities))
 
 
 
